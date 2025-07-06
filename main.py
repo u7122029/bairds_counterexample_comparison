102f1dc26283a31c2155395914be9094f973afd0
u7122029 <u7122029@anu.edu.au>
2025-07-06 20:39:47 +1000

    Properly added seeding for reproducibility.

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ import bairds_counterexample_env
 from gymnasium.wrappers import TimeLimit
 from collections import deque
 import matplotlib.pyplot as plt
+from pathlib import Path
+
 
 class Agent:
-    def __init__(self, no_states, no_actions):
+    def __init__(self, no_states, no_actions, seed=42):
+        self.np_random = np.random.default_rng(seed)
+
         self.no_states = no_states
         self.no_actions = no_actions
 
@@ -57,7 +61,7 @@ class Agent:
 
     def step(self, observation, reward, terminated):
         # sampling from behaviour policy.
-        action = np.random.choice(self.no_actions, p=[1 - 1/self.no_states, 1/self.no_states])
+        action = self.np_random.choice(list(range(self.no_actions)), p=[1 - 1/self.no_states, 1/self.no_states])
         if self.train:
             self.trajectory.extend([observation, reward, terminated, action])
             if len(self.trajectory) >= 8:
@@ -77,8 +81,8 @@ class Agent:
         pass
 
 
-def play_episode(env, agent):
-    state, _ = env.reset()
+def play_episode(env, agent, seed=42):
+    state, _ = env.reset(seed=seed)
     agent.reset(True)
 
     terminated, reward = 0, False
@@ -86,8 +90,8 @@ def play_episode(env, agent):
     done = False
 
     while not done:
-        agent.step(state, reward, terminated)
-        state, reward, terminated, truncated, _ = env.step(env.action_space.sample())
+        action = agent.step(state, reward, terminated)
+        state, reward, terminated, truncated, _ = env.step(action)
         episode_reward += reward
         elapsed_steps += 1
         done = terminated or truncated
@@ -96,19 +100,25 @@ def play_episode(env, agent):
 
 
 def main():
-    max_steps = 500
-    num_states = 3
+    max_steps = 2000
+    num_states = 2
     env = TimeLimit(gym.make('bairds_counterexample-v0', num_intermediate_states=num_states - 1),
                     max_episode_steps=max_steps)
-    agent = Agent(env.observation_space.n, env.action_space.n)
+    agent = Agent(env.observation_space.n, env.action_space.n, 42)
     play_episode(env, agent)
     full_w_history = np.stack(agent.w_history)
 
-    for i in range(num_states):
+    for i in range(num_states + 1):
+        out_path = Path("figures") / str(num_states).zfill(2) / f"w_{i}.png"
+        out_path.parent.mkdir(parents=True, exist_ok=True)
         plt.figure()
+        plt.xlabel("Steps")
+        plt.ylabel("Weight")
+        #if num_states > 2:
         plt.yscale('symlog')
         plt.plot(full_w_history[:, i])
-        plt.show()
+        plt.title(f"Parameter w_{i} over {num_states} States")
+        plt.savefig(out_path)
 
 if __name__ == "__main__":
     main()
